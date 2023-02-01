bd05aefee37cc56ae59faece10fbd9355b41f6b0
BookStoreGroup5 <123921441+BookStoreGroup5@users.noreply.github.com>
2023-02-01 18:11:55 +0530

    Add files via upload

---
+++ IBookRepository.java
@@ -0,0 +1,21 @@
+package com.cg.bookstoreapplication.repository;
+
+import java.util.List;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cg.bookstoreapplication.entities.Book;
+@Repository
+public interface IBookRepository extends JpaRepository<Book,Integer> {
+	
+	public Book findByTitleAndAuthor(String title,String author);
+	public Book findByTitle(String title);
+	
+	public List<Book> findByCategory(String category);
+	
+	
+	//public List<Book> listBestSellingBook();
+}
